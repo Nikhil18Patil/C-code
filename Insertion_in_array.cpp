72f2a4ec91e340f5b1b5a9dd130d49af86f34546
TaniyaBoth <119886465+TaniyaBoth@users.noreply.github.com>
2022-12-05 23:42:31 +0530

    Add files via upload

---
+++ Insertion_in_array.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[100],n,value,index;
+    cout<<"enter the number of element :"<<endl;
+    cin>>n;
+    for(int i=0;i<=n-1;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"*******before insertion :******** "<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<"\n enter the index and value that u want to enter "<<endl;
+    cin>>index>>value;
+    for(int i=n-1;i>=index;i--){
+arr[i+1]=arr[i];
+    }
+
+arr[index]=value;
+    n=n+1;
+    cout<<"**********after insertion********* "<<endl;
+    for(int i=0;i<=n-1;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<"complete programme ::"<<endl;
+return 0;
+}
